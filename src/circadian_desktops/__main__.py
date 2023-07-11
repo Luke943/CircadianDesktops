cb90ddaf0d3fd628fa389b72064e51cf800c8996
Luke Hartley <luke.hartley05@gmail.com>
2023-07-11 09:27:42 +0100

    changes for py/pyw compatibility

--- src/circadian_desktops/__main__.py
+++ src/circadian_desktops/__main__.py
@@ -9,13 +9,9 @@ import os
 import subprocess
 import sys
 
-exe_dir, exe_name = os.path.split(sys.executable)
-if not exe_name.startswith("pythonw"):
-    exe_name = exe_name.replace("python", "pythonw", 1)
-    exe_path = os.path.join(exe_dir, exe_name)
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 if "/noshow" in sys.argv:
-    subprocess.Popen([exe_path, "app.py", "/noshow"])
+    subprocess.Popen([sys.executable, "app.pyw", "/noshow"])
 else:
-    subprocess.Popen([exe_path, "app.py"])
+    subprocess.Popen([sys.executable, "app.pyw"])
 sys.exit(0)
