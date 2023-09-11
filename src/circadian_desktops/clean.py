a456ef6b41384e9dc04dfac10cb0284f71c6f04c
Luke Hartley <luke.hartley05@gmail.com>
2023-09-11 10:43:25 +0100

    Edit import syntax

--- src/circadian_desktops/clean.py
+++ src/circadian_desktops/clean.py
@@ -6,8 +6,8 @@ These would not be removed by a pip uninstall.
 if __name__ == "__main__":
     import os
 
-    from circadian_desktops.functions import run_on_startup
-    from circadian_desktops.app import settingsFile
+    from .functions import run_on_startup
+    from .app import settingsFile
 
     run_on_startup(False)
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
